d115f6b7b06c83c92e8e05ae40bb4e3880d81f96
Lia Ferguson <lkferguson7@gmail.com>
2021-04-02 14:15:24 -0400

    temporarily revert to old endpoint.py file.

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -2,7 +2,6 @@ from flask import Flask, Blueprint, request, jsonify;
 from app.db import get_db
 
 # lines 5-33 written by Lia Ferguson
-# Edited and supplemeneted by Andrew Fecher
 bp = Blueprint('endpoints', __name__, url_prefix='/endpoints')
 
 NUM_RECORDS_USERS_TABLE = 7 # replace this number with actual number of records in database
@@ -27,39 +26,12 @@ def login_query():
 	
 	login_q = 'SELECT * FROM USERS WHERE User_ID = {quote}{u_id} AND Password = {pwd}'.format(quote=quote, u_id = user_id, pwd = password)
 
-	commands = login_q.split(";", -1)
-	query_results = list()
-	error = ''
-	try:
-		for command in commands:
-			query_results.append(database.execute(command).fetchall())
-	except Exception as e:
-		query_results.clear()
-		error = e.args
-	
-	contents = ''
-	i = 0;
-	if len(query_results) == len(commands):
-		for table in query_results:
-			contents += 'Query #' + str(i) + ' Results\n'
-			i += 1
-			for row in table:
-				for element in row:
-					contents += str(element).replace('\n','/n').replace('\t','/t').replace('\r','/r') + '\t'
-				contents += '\n'
-			contents += '\n\n'
-	else:
-		contents = error
-
-	query_result = '';
-	if len(query_results) > 0:
-		query_result = query_results[0]
-	if len(query_result) > 0:
+	query_result = database.execute(login_q).fetchall()
+	response = {}
+	if len(query_result) == NUM_RECORDS_USERS_TABLE:
 		response = {
 			'isQuerySuccessful': 'true',
 			'status': 'SUCCESS',
-			'results': contents,
-			'login': query_result[0][0],
 			'message': 'Congratulations! You successfully used SQL Injection to bypass authentication.'
 		}
 
@@ -67,12 +39,11 @@ def login_query():
 		response = {
 			'isQuerySuccessful': 'false',
 			'status': 'ERROR',
-			'results': contents,
-			'login': 'ERROR',
 			'message': 'SQL Injection was not successful, please try again.'
 		}
-	#print(contents)
 	print(response)
 	print(user_id)
 	return jsonify(response)
+
+
 
